a76f8b6e077a1f9b992dd8226e21ebb4235a57f9
Felddy <markf@geekpad.com>
2019-07-05 18:29:09 -0400

    Add test for options_first config of docopt

--- tests/test_sherpa.py
+++ tests/test_sherpa.py
@@ -38,7 +38,7 @@ def test_release_version():
 
 def test_child_timeout_kill(capsys):
     """Determine if the sherpa killed the child, and it exited with non-zero."""
-    with patch.object(sys, "argv", ["bogus", "--timeout=1s", "sleep 10"]):
+    with patch.object(sys, "argv", ["bogus", "--timeout=1s", "sleep", "10"]):
         return_code = sherpa.main()
         captured = capsys.readouterr()
         assert (
@@ -49,7 +49,7 @@ def test_child_timeout_kill(capsys):
 
 def test_child_success(capsys):
     """Determine if the sherpa killed the child, and it exited with non-zero."""
-    with patch.object(sys, "argv", ["bogus", "--timeout=5s", "sleep 3"]):
+    with patch.object(sys, "argv", ["bogus", "--timeout=5s", "sleep", "3"]):
         return_code = sherpa.main()
         captured = capsys.readouterr()
         assert (
@@ -61,7 +61,7 @@ def test_child_success(capsys):
 @pytest.mark.slow
 def test_child_slow_success(capsys):
     """Determine if the sherpa killed the child, and it exited with non-zero."""
-    with patch.object(sys, "argv", ["bogus", "--timeout=2m", "sleep 90"]):
+    with patch.object(sys, "argv", ["bogus", "--timeout=2m", "sleep", "90"]):
         return_code = sherpa.main()
         captured = capsys.readouterr()
         assert (
@@ -72,7 +72,7 @@ def test_child_slow_success(capsys):
 
 def test_child_fast_success(capsys):
     """Determine if the sherpa killed the child, and it exited with non-zero."""
-    with patch.object(sys, "argv", ["bogus", "--timeout=1", "exit 0"]):
+    with patch.object(sys, "argv", ["bogus", "--timeout=1", "exit", "0"]):
         return_code = sherpa.main()
         captured = capsys.readouterr()
         assert (
@@ -83,7 +83,7 @@ def test_child_fast_success(capsys):
 
 def test_child_fast_fail(capsys):
     """Determine if the sherpa killed the child, and it exited with non-zero."""
-    with patch.object(sys, "argv", ["bogus", "--timeout=1", "exit 1"]):
+    with patch.object(sys, "argv", ["bogus", "--timeout=1", "exit", "1"]):
         return_code = sherpa.main()
         captured = capsys.readouterr()
         assert (
@@ -95,6 +95,24 @@ def test_child_fast_fail(capsys):
 def test_bad_timeout(capsys):
     """Test passing in a bad timeout value."""
     with pytest.raises(SystemExit):
-        with patch.object(sys, "argv", ["bogus", "--timeout=yourmom", "exit 1"]):
+        with patch.object(sys, "argv", ["bogus", "--timeout=yourmom", "exit", "1"]):
             return_code = sherpa.main()
             assert return_code == 1, "main() should return 1 when timeout is invalid"
+
+
+def test_child_with_flags(capsys):
+    """Test a child command that has flags and arguments.
+
+    This tests to make sure options_first=True is on for docopt.
+    """
+    with patch.object(
+        sys,
+        "argv",
+        ["bogus", "--timeout=2", "echo", "--something", "more", "complicated"],
+    ):
+        return_code = sherpa.main()
+        captured = capsys.readouterr()
+        assert (
+            "exited with: 0" in captured.out
+        ), "didn't see expected 'exited with: 0' message in output"
+        assert return_code == 0, "main() should return 0 when the child returns 0"
