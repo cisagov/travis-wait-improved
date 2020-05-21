adacc4988fe7a9ea219e1212b7806178c7781a92
Felddy <markf@geekpad.com>
2020-05-20 21:54:13 -0400

    Add a type for __init__ __all__.

--- src/travis_wait_improved/__init__.py
+++ src/travis_wait_improved/__init__.py
@@ -1,4 +1,7 @@
 """The travis_wait_improved library."""
+# Standard Python Libraries
+from typing import List
+
 from ._version import __version__  # noqa: F401
 
-__all__ = []
+__all__: List[str] = []
